8a9e46dfcc3a1625cb965d2c326c54c22311a10e
eventures-io <psymoens@eventures.io>
2014-08-28 23:46:42 +0200

    customize greensock-site

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-angular.module('website', [])
+angular.module('website', ['ngAnimate'])
+    .constant('TweenMax', TweenMax)
     .controller('MainCtrl', function ($scope) {
         $scope.pages = {
             'home': { label: 'Home', sublabel: 'Sublabel', content: 'This is page content.' },
@@ -8,17 +9,23 @@ angular.module('website', [])
 
         $scope.currentPage = 'home';
         $scope.page = $scope.pages['home'];
+        $scope.isInTransit = false;
 
         $scope.setCurrentPage = function (page) {
             if ($scope.currentPage !== page) {
                 $scope.page = $scope.pages[page];
                 $scope.currentPage = page;
+                $scope.isInTransit = true;
             }
         };
 
         $scope.isCurrentPage = function (page) {
             return $scope.currentPage === page;
         };
+
+        $scope.$on('bgTransitionComplete', function(){
+            $scope.isInTransit = false;
+        });
     })
     .directive('bg', function ($window) {
         // Adapted from http://bavotasan.com/2011/full-sizebackground-image-jquery-plugin/ Thanks @bavotasan!
@@ -58,6 +65,43 @@ angular.module('website', [])
         return {
             restrict: 'A',
             link: linker
+        };
+    })
+    .animation('.bg-animation', function ($window, $rootScope, TweenMax) {
+        return {
+            enter: function (element, done) {
+                TweenMax.fromTo(element, 0.5, { top: $window.innerHeight}, {top: 0, onStart: function () {
+                    //onStart: start the panel-animation when the bg-animation starts. alternative: onComplete
+                    $rootScope.$apply(function(){
+                        $rootScope.$broadcast('bgTransitionComplete');
+                    });
+                    done();
+                }});
+            },
+            leave: function (element, done) {
+                TweenMax.to(element, 0.5, {top: -$window.innerHeight, onComplete: done});
+            }
+        };
+    })
+    .animation('.panel-animation', function (TweenMax) {
+        return {
+            addClass: function (element, className, done) {
+                if (className == 'ng-hide') {
+                    TweenMax.to(element, 0.4, { opacity: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+                    TweenMax.fromTo(element, 0.5, { opacity: 0, top: -element.height() }, { opacity: 0.8, top: (window.innerHeight*60)/100 , onComplete: done });
+                }
+                else {
+                    done();
+                }
+            }
         };
     });
 
